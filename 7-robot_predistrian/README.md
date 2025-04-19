701ff7affaf431c7600bfe1d9c72d67ac539ad6b
SinekaRamu <sineka2001cud@gmail.com>
2025-04-19 11:44:54 +0530

    added oled with robot

--- 7-robot_predistrian/README.md
+++ 7-robot_predistrian/README.md
@@ -1,6 +1,6 @@
 # ROBOT WITH PREDISTRIAN DETECTION
 
-Robot will move according to voice send from bluetooth while moving forware if it detects any object then picture is taken using raspberry pi camera and the object is identified using raspberrypi. if the person is detected then message is send to esp32 and audio message will be played along with that. 
+Robot will move according to voice send from bluetooth while moving forware if it detects any object then picture is taken using raspberry pi camera and the object is identified using raspberrypi. if the person is detected then message is send to esp32 and audio message will be played along with that.
 
 ## Components Required
 
@@ -18,4 +18,24 @@ Robot will move according to voice send from bluetooth while moving forware if i
 
 ## connection for ESP32
 
-- 
+- Ultrasonic sensor
+
+  - trigFront GPIO 22
+  - echoFront GPIO 23
+  - trigBack GPIO 22
+  - echoBack GPIO 23
+
+- Motor Control
+
+  - ENA 16 (RX2) // Left motor speed control (must be PWM capable)
+  - ENB 17 (TX2) // Right motor speed control (must be PWM capable)
+  - IN1 5 (D5) // Left motor forward
+  - IN2 18 (D18) // Left motor backward
+  - IN3 19 (D19) // Right motor forward
+  - IN4 23 (D15) // Right motor backward
+
+- OLED display
+  - Vin - 3.3V
+  - GND - Gnd
+  - SDA - D21
+  - SCL - D22
--- 7-robot_predistrian/robot.ino/robot.ino.ino
+++ 7-robot_predistrian/robot.ino/robot.ino.ino
@@ -1,34 +1,58 @@
 #include "BluetoothSerial.h"
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to enable it
 #endif
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define OLED_RESET -1 // Reset pin (not used)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 BluetoothSerial SerialBT;
 
 // Motor control pins
-#define ENA 16  // Left motor speed control (must be PWM capable)
-#define ENB 17  // Right motor speed control (must be PWM capable)
-#define IN1 5   // Left motor forward
-#define IN2 18  // Left motor backward
-#define IN3 19  // Right motor forward
-#define IN4 21  // Right motor backward 
+#define ENA 16 // Left motor speed control (must be PWM capable)
+#define ENB 17 // Right motor speed control (must be PWM capable)
+#define IN1 5  // Left motor forward
+#define IN2 18 // Left motor backward
+#define IN3 19 // Right motor forward
+#define IN4 23 // Right motor backward
 
-const int trigPin = 22;
-const int echoPin = 23;
+const int trigPin = 2;
+const int echoPin = 4;
 long duration;
 int distance;
 
 // setting PWM properties
 const int freq = 1000;
 const int resolution = 8;
- 
-void setup() {
+
+void setup()
+{
     Serial.begin(115200);
     SerialBT.begin("ESP32-BT-Robot");
 
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    {
+        Serial.println(F("OLED failed"));
+        for (;;)
+            ; // Don't proceed
+    }
+
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    display.println("ESP32 Ready");
+    display.display();
+    delay(5000);
+
     pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-    pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+    pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
 
     // Set motor control pins as outputs
     pinMode(ENA, OUTPUT);
@@ -44,10 +68,26 @@ void setup() {
     // Initialize motors in stopped state
     stopMotors();
 
+    display.setCursor(0, 12);
+    display.print("Bluetooth name:");
+    display.setCursor(0, 24);
+    display.println("ESP32-BT-Robot");
+    display.display();
+
     Serial.println("Bluetooth Robot Ready!");
 }
 
-void loop() {
+void updateDisplay(String message)
+{
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Status:");
+    display.println(message);
+    display.display();
+}
+
+void loop()
+{
 
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
@@ -58,31 +98,50 @@ void loop() {
     // Reads the echoPin, returns the sound wave travel time in microseconds
     duration = pulseIn(echoPin, HIGH);
     // Calculating the distance
-    distance= duration*0.034/2;
+    distance = duration * 0.034 / 2;
     // Prints the distance on the Serial Monitor
     Serial.print("Distance: ");
     Serial.println(distance);
     delay(50);
 
-    if (distance <= 10){
-      stopMotors();
+    if (distance <= 10)
+    {
+        stopMotors();
     }
 
-    if (SerialBT.available()) {
+    if (SerialBT.available())
+    {
         char cmd = SerialBT.read();
         Serial.println(cmd);
 
-        switch (cmd) {
-            case 'f': moveForward(); break;
-            case 'b': moveBackward(); break;
-            case 'l': turnLeft(); break;
-            case 'r': turnRight(); break;
-            case 's': stopMotors(); break;
+        switch (cmd)
+        {
+        case 'w':
+            updateDisplay("Moving Forward");
+            moveForward();
+            break;
+        case 's':
+            updateDisplay("Moving Backward");
+            moveBackward();
+            break;
+        case 'a':
+            updateDisplay("Moving Left");
+            turnLeft();
+            break;
+        case 'd':
+            updateDisplay("Moving Right");
+            turnRight();
+            break;
+        case 'x':
+            updateDisplay("Moving Stop");
+            stopMotors();
+            break;
         }
     }
-} 
+}
 
-void moveForward() {
+void moveForward()
+{
     ledcWrite(ENA, 120); // Left motor speed (0-255)
     ledcWrite(ENB, 120); // Right motor speed (0-255)
     digitalWrite(IN1, HIGH);
@@ -91,7 +150,8 @@ void moveForward() {
     digitalWrite(IN4, LOW);
 }
 
-void moveBackward() {
+void moveBackward()
+{
     ledcWrite(ENA, 80);
     ledcWrite(ENB, 80);
     digitalWrite(IN1, LOW);
@@ -100,7 +160,8 @@ void moveBackward() {
     digitalWrite(IN4, HIGH);
 }
 
-void turnLeft() {
+void turnLeft()
+{
     ledcWrite(ENA, 80);
     ledcWrite(ENB, 80);
     digitalWrite(IN1, LOW);
@@ -109,7 +170,8 @@ void turnLeft() {
     digitalWrite(IN4, LOW);
 }
 
-void turnRight() {
+void turnRight()
+{
     ledcWrite(ENA, 80);
     ledcWrite(ENB, 80);
     digitalWrite(IN1, HIGH);
@@ -118,7 +180,8 @@ void turnRight() {
     digitalWrite(IN4, HIGH);
 }
 
-void stopMotors() {
+void stopMotors()
+{
     ledcWrite(ENA, 0);
     ledcWrite(ENB, 0);
     digitalWrite(IN1, LOW);
